528e9375affbffae296ac9c5efc513f629415eaf
Winter LaMon <winter.lamon@gmail.com>
2017-10-01 19:11:30 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ def monopoly_with_third_tier
 		railroads: {
 			pieces: 4,
 			names: {
-				reading_railroad: {}, 
+				reading_railroad: {},
 				pennsylvania_railroad: {}
 			},
 			rent_in_dollars: {
