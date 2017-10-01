c1f4f5d61f55cba32fb5d7ab9ddfcd7221de1bd0
Winter LaMon <winter.lamon@gmail.com>
2017-10-01 19:04:34 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,14 +1,13 @@
 def new_hash
-
+	{}
 end
 
 def actor
-
+	actor = {name: "Dwayne The Rock Johnson"}
 end
 
 def monopoly
-	monopoly = {}
-
+	monopoly = {railroads: {}}
 end
 
 def monopoly_with_second_tier
